ae3531d6fff746b1b911d7c7a0441daff033f8ff
frank <lovefree103@gmail.com>
2021-10-01 12:43:20 +0800

    fixed `load more` issue for transaction history

--- src/status_im/wallet/core.cljs
+++ src/status_im/wallet/core.cljs
@@ -662,6 +662,9 @@
      #(re-frame.core/dispatch [::restart])
      interval)))
 
+(defn- reset-all-fetched? [db addresses]
+  (reduce #(assoc-in %1 [:wallet :fetching %2 :all-fetched?] :all-preloaded) db addresses))
+
 (fx/defn check-recent-history
   [{:keys [db] :as cofx}
    {:keys [on-recent-history-fetching force-restart?]}]
@@ -674,7 +677,8 @@
                         (assoc :wallet-service/restart-timeout timeout
                                :wallet-service/custom-interval (get-next-custom-interval db)
                                :wallet/was-started? true
-                               :wallet/on-recent-history-fetching on-recent-history-fetching))
+                               :wallet/on-recent-history-fetching on-recent-history-fetching)
+                        (reset-all-fetched? addresses))
      ::check-recent-history addresses
      ::utils.utils/clear-timeouts
      [(when (not= timeout old-timeout) old-timeout)]}))
